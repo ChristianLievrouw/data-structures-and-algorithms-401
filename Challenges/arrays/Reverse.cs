407bc1360d96cf76298a4f567a658a86afffaed3
ChristianLievrouw <celievrouw1@gmail.com>
2020-08-24 15:23:33 -0500

    challenge done

---
+++ Challenges/arrays/Reverse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Challenges;
+namespace Challenges.Arrays
+{
+    class Reverse
+    {
+        public static int[] ReverseArray(int[] array)
+        {
+            int[] reversedArray = new int[array.Length];
+            int reverseSpot = array.Length - 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                reversedArray[reverseSpot] = array[i];
+                reverseSpot--;
+            }
+            return reversedArray;
+        }
+    }
+}
