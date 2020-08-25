c4b444af903769b3e485c231042916ff31a0d16a
ChristianLievrouw <celievrouw1@gmail.com>
2020-08-25 15:24:02 -0500

    added code challange 2 and updated README

---
+++ Challenges.tests/arrays/ReverseTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Challenges.Arrays;
+using Xunit;
+
+namespace Challenges.tests.arrays
+{
+    public class ReverseTests
+    {
+        [Fact]
+        public void ReverseArray_reverses_an_empty_array()
+        {
+            // Arrange
+            int[] arrayToReverse = new[] { 1, 2, 3 };
+
+            // Act
+            int[] result = Reverse.ReverseArray(arrayToReverse);
+
+            //Assert
+            Assert.Equal(new int[] { 3, 2, 1 }, result);
+        }
+    }
+}
--- Challenges/arrays/Reverse.cs
+++ Challenges/arrays/Reverse.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Challenges;
 namespace Challenges.Arrays
 {
-    class Reverse
+    public class Reverse
     {
         public static int[] ReverseArray(int[] array)
         {
