a4e5be88aa1cd7a588a8f53c5347b8f03f16251e
ChristianLievrouw <celievrouw1@gmail.com>
2020-08-26 15:03:30 -0500

    updated README and finished code challange 3

---
+++ Challenges.tests/arrays/BinaryTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Challenges.arrays;
+using Xunit;
+
+namespace Challenges.tests.arrays
+{
+    public class BinaryTests
+    {
+        [Theory]
+        [InlineData(new[] {1, 2, 3, 4, 5, 6}, 2, 1)]
+        public void BinarySearch_search_for_input_num_in_array(int[] testArray, int testNum, int expected)
+        {
+            // Arrange
+            // Act
+            int result = BinarySearch.Search(testArray, testNum);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Challenges/arrays/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Challenges.arrays
+{
+    public class BinarySearch
+    {
+        public static int Search(int[] array, int num)
+        {
+            int minNum = 0;
+            int maxNum = array.Length - 1;
+
+            while (minNum <= maxNum)
+            {
+                int mid = (minNum + maxNum) / 2;
+                if (num == array[mid])
+                {
+                    return mid;
+                }
+                else if (num < array[mid])
+                {
+                    maxNum = mid - 1;
+                }
+                else if (num > array[mid])
+                {
+                    minNum = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
