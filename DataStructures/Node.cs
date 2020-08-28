7d06578149eb611380d03a29e34b700effd50a61
ChristianLievrouw <celievrouw1@gmail.com>
2020-08-28 17:18:16 -0500

    finied code challenge

---
+++ DataStructures/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Node
+    {
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        // Value of this node
+        public int Value { get; set; }
+
+        // Pointer to the next node in the list
+        public Node Next { get; set; }
+    }
+}
