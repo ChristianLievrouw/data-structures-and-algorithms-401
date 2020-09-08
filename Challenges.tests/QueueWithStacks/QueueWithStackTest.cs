83c3517b4599e535f888fcaadbd4d84a0a11b4de
ChristianLievrouw <celievrouw1@gmail.com>
2020-09-08 15:55:36 -0500

    starting text

---
+++ Challenges.tests/QueueWithStacks/QueueWithStackTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Challenges.QueueWithStacks;
+using DataStructures.Stack;
+using Xunit;
+
+namespace Challenges.tests.QueueWithStacks
+{
+    public class QueueWithStackTest
+    {
+        [Fact]
+        public void IsEmpty_returns_true_if_stack_is_empty()
+        {
+            // Arrange 
+            Stack<int> stack = new Stack<int>();
+
+            // Act
+            bool result = stack.IsEmpty();
+
+            // Assert
+            Assert.True(result);
+
+        }
+        //[Fact]
+        //public void Dequeue_a_stack()
+        //{
+        //    Stack<int> stack = new Stack<int>();
+
+        //    stack.Push(3);
+        //    stack.Push(2);
+        //    stack.Push(1);
+
+        //    Stack<int> stack2 = new Stack<int>();
+
+        //    Assert.Equal(3, stack2.Peek());
+        //}
+
+        //[Fact]
+        //public void Enqueue_a_stack()
+        //{
+        //    Stack<int> stack = new Stack<int>();
+
+        //    stack.Push(3);
+        //    stack.Push(2);
+        //    stack.Push(1);
+        //}
+    }
+}
---
+++ Challenges/QueueWithStacks/QueueWithStacks.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using DataStructures.Stack;
+
+
+namespace Challenges.QueueWithStacks
+{
+    public class QueueWithStacks<T> : Stack<T>
+    {
+        Stack<int> stack = new Stack<int>();
+        Stack<int> stack2 = new Stack<int>();
+
+        public void Enqueue(T value)
+        {
+            if(stack.IsEmpty() != true)
+            {
+                stack2.Push(stack.Pop());
+
+            }
+            stack.Push(4);
+
+            while (stack2.IsEmpty() != true)
+            {
+                stack.Push(stack2.Pop());
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (stack.IsEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+            }
+
+            int value = stack.Peek();
+            stack.Pop();
+            return value;
+        }
+
+        public class Node
+        {
+
+            public Node(int value)
+            {
+                Value = value;
+            }
+
+            // Value of this node
+            public int Value { get; set; }
+
+            // Pointer to the next node in the list
+            public Node Next { get; set; }
+        }
+    }
+}
+    
+
+
