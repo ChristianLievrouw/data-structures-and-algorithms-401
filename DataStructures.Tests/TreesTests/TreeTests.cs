b8b59b90ac24d4e8fb7abbae196d03f105dc0385
ChristianLievrouw <celievrouw1@gmail.com>
2020-09-13 19:53:43 -0500

    added BinarySearch

---
+++ DataStructures.Tests/TreesTests/TreeTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DataStructures.Trees;
+using Xunit;
+
+namespace DataStructures.Tests.TreesTests
+{
+    public class TreeTests
+    {
+        [Fact]
+        public void Add_Binary_Tree_can_add_multiple_nodes()
+        {
+            // Arrange
+            BinarySearchTree tree = new BinarySearchTree();
+            // Act
+            tree.Add(6);
+
+            //Assert
+            Assert.Equal(6, tree.Root.Value);
+           
+        }
+    }
+}
---
+++ DataStructures/Trees/BinarySearchTree.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+namespace DataStructures.Trees
+{
+    public class Node
+    {
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; set; }
+
+        public Node Left { get; set; }
+
+        public Node Right { get; set; }
+    }
+
+    public class BinarySearchTree
+    {
+        public Node Root { get; set; }
+
+        public void Add(int value)
+        {
+            Node newNode = new Node(value);
+            if (Root == null)
+                Root = newNode;
+            else
+            {
+                Node current = Root;
+                Node Prev;
+                while (true)
+                {
+                    Prev = current;
+                    if (value < current.Value) //go left
+                    {
+                        current = current.Left;
+                        if (current == null) //add new node if left is null
+                        {
+                            Prev.Left = newNode;
+                            return;
+                        }
+                    }
+                    else //go right
+                    {
+                        current = current.Right;
+                        if (current == null) //add new node if Right is null
+                        {
+                            Prev.Right = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool Contains(Node current, int value)
+        {
+            if (Root == null || current == null)
+                return false;
+            else
+            {
+                if (current.Value == value)
+                    return true;
+                else if (value < current.Value)
+                    return Contains(current.Left, value);
+                else
+                    return Contains(current.Right, value);
+            }
+        }
+    }
+}
+
+
