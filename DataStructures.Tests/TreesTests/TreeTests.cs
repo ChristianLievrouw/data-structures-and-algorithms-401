1be31d81696ac015be1799b66c8eb10044c88af1
ChristianLievrouw <celievrouw1@gmail.com>
2020-09-14 15:36:29 -0500

    fixing

--- DataStructures.Tests/TreesTests/TreeTests.cs
+++ DataStructures.Tests/TreesTests/TreeTests.cs
@@ -19,24 +19,5 @@ namespace DataStructures.Tests.TreesTests
             Assert.Equal(6, tree.Root.Value);
            
         }
-
-        [Fact]
-        public void FindFindMaxValue_returns_max_int()
-        {
-            // Arrange
-            BinarySearchTree tree = new BinarySearchTree();
-            tree.Add(6);
-            tree.Add(2);
-            tree.Add(7);
-            tree.Add(3);
-            tree.Add(10);
-            tree.Add(1);
-            tree.Add(9);
-            tree.Add(20);
-            // Act
-            int result = tree.FindMax(tree.Root);
-            // Assert
-            Assert.Equal(20, result);
-        }
     }
 }
