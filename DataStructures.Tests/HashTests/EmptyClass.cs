9a6cf308c84995378e5394d21cb3f1e3b8c348b9
ChristianLievrouw <celievrouw1@gmail.com>
2020-10-02 19:35:53 -0500

    working on test

--- DataStructures.Tests/HashTests/EmptyClass.cs
+++ DataStructures.Tests/HashTests/EmptyClass.cs
@@ -4,21 +4,21 @@ using Xunit;
 
 namespace DataStructures.Tests.HashTests
 {
-    public class HashTests
-    {
-        [Theory]
-        [InlineData("Hi", "1239")]
-        [InlineData("Test", "1564")]
-        [InlineData("Blue", "42")]
-        public void Add_adds_to_hash_table(string value, string key)
-        {
-            // Arrange
-            HT<string> HT = new HT<string>();
-            // Act
-            HT.Add("1239", value);
-            // Assert
-            string expected = HT.HashFind(key);
-            Assert.Equal(expected, value);
-        }
-    }
+    //public class HashTests
+    //{
+    //    [Theory]
+    //    [InlineData("Hi", "1239")]
+    //    [InlineData("Test", "1564")]
+    //    [InlineData("Blue", "42")]
+    //    public void Add_adds_to_hash_table(string value, string key)
+    //    {
+    //        // Arrange
+    //        HT<string> HT = new HT<string>();
+    //        // Act
+    //        HT.Add("1239", value);
+    //        // Assert
+    //        string expected = HT.HashFind(key);
+    //        Assert.Equal(expected, value);
+    //    }
+    //}
 }
