48130315b0fee2c52a776bee064419d10f3d7b8f
Keith Dahlby <keith@newbo.co>
2020-10-07 11:45:34 -0500

    Skip test instead of comment it out

--- DataStructures.Tests/HashTests/IntersectionTests.cs
+++ DataStructures.Tests/HashTests/IntersectionTests.cs
@@ -1,29 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using DataStructures.HashTable;
 using DataStructures.Trees;
 using Xunit;
 
 namespace DataStructures.Tests.HashTests
 {
-    //public class IntersectionTests
-    //{
-    //    [Fact]
-    //    public void Bianary_tree_repeat()
-    //    {
-    //        BinaryTree<int> tree1 = new BinaryTree<int>();
-    //        BinaryTree<int> tree2 = new BinaryTree<int>();
-    //        tree1.Root = new BinaryTree<int>.Node(1);
-    //        tree2.Root = new BinaryTree<int>.Node(1);
-    //        tree1.Root.Left = new BinaryTree<int>.Node(2);
-    //        tree2.Root.Left = new BinaryTree<int>.Node(2);
-    //        tree1.Root.Right = new BinaryTree<int>.Node(3);
-    //        tree2.Root.Right = new BinaryTree<int>.Node(4);
-    //        List<int> value = Intersection<int>.TreeIntersection(tree1, tree2);
-    //        List<int> expected = new List<int>();
-    //        expected.Add(1);
-    //        expected.Add(2);
-    //        Assert.Equal(expected, value);
-    //    }
-    //}
+    public class IntersectionTests
+    {
+        [Fact]
+        public void Bianary_tree_repeat()
+        {
+            BinaryTree<int> tree1 = new BinaryTree<int>();
+            tree1.Root = new BinaryTree<int>.Node(1);
+            tree1.Root.Left = new BinaryTree<int>.Node(2);
+            tree1.Root.Right = new BinaryTree<int>.Node(3);
+
+            BinaryTree<int> tree2 = new BinaryTree<int>();
+            tree2.Root = new BinaryTree<int>.Node(1);
+            tree2.Root.Left = new BinaryTree<int>.Node(2);
+            tree2.Root.Right = new BinaryTree<int>.Node(4);
+
+            List<int> value = Intersection<int>.TreeIntersection(tree1, tree2);
+            List<int> expected = new List<int>();
+            expected.Add(1);
+            expected.Add(2);
+            Assert.Equal(expected, value);
+        }
+    }
 }
